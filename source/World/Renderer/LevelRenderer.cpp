cf6d27f12310e857a181f2dc9e685e95d740e28e
Vruk <BlueDiamondBoy11@gmail.com>
2023-07-31 05:50:26 -0700

    Create screenshots folder if it doesn't exist
    When taking a screenshot the windows implementation will now check for a folder to save to and create it if it does not exist
    
    g_pApp->field_D58 is now g_pApp->m_externalStorageDir

--- source/World/Renderer/LevelRenderer.cpp
+++ source/World/Renderer/LevelRenderer.cpp
@@ -1031,9 +1031,9 @@ void LevelRenderer::takePicture(TripodCamera* pCamera, Entity* pOwner)
 	static char str[256];
 	// @HUH: This has the potential to overwrite a file
 #ifdef ORIGINAL_CODE
-	sprintf(str, "%s/games/com.mojang/img_%.4d.jpg", m_pMinecraft->field_D58.c_str(), getTimeMs());
+	sprintf(str, "%s/games/com.mojang/img_%.4d.jpg", m_pMinecraft->m_externalStorageDir.c_str(), getTimeMs());
 #else
-	sprintf(str, "%s/games/com.mojang/img_%.4d.png", m_pMinecraft->field_D58.c_str(), getTimeMs());
+	sprintf(str, "img_%.4d.png", getTimeMs());
 #endif
 
 	m_pMinecraft->platform()->saveScreenshot(std::string(str), Minecraft::width, Minecraft::height);
