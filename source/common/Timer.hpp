a86ed83c9c8550c4df425885a3cacdad34e8ca59
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2023-08-20 20:41:04 +0300

    Update Timer.hpp

--- source/common/Timer.hpp
+++ source/common/Timer.hpp
@@ -20,19 +20,19 @@ public:
 	void advanceTime();
 
 public:
-	float field_0;
+	float lastUpdateTime;
 #ifndef USE_ACCURATE_TIMER
-	int field_4;
-	int field_8;
+	int lastSyncTime;
+	int unprocessedTime;
 #else
-	double field_4;
-	double field_8;
+	double lastSyncTime;
+	double unprocessedTime;
 #endif
-	float field_C;
-	float field_10;
-	int field_14;
-	float field_18;
-	float field_1C;
-	float field_20;
+	float tickAdjustment;
+	float ticksPerSecond;
+	int ticks;
+	float renderTicks;
+	float timerSpeed;
+	float partialTicks;
 };
 
