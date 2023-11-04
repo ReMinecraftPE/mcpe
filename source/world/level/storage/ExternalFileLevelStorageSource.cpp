b724d7a62ccfea1b4fa18704fcf4fbbcb5358acc
SanyaSho <68691958+SanyaSho@users.noreply.github.com>
2023-11-04 23:47:33 +0300

    ExternalFileLevelStorageSource: use scary stdstring instead of char

--- source/world/level/storage/ExternalFileLevelStorageSource.cpp
+++ source/world/level/storage/ExternalFileLevelStorageSource.cpp
@@ -59,12 +59,10 @@ void ExternalFileLevelStorageSource::getLevelList(std::vector<LevelSummary>& vls
 		LOG_I("Entry: %s", de->d_name);
 
 #if defined( __HAIKU__ )
-		// @HACK: make a temp buffer here
-		char tempbuf[PATH_MAX];
-		snprintf( tempbuf, sizeof( tempbuf ), "%s/%s", m_worldsPath.c_str(), de->d_name );
+		std::string temp = m_worldsPath + '/' + de->d_name;
 
 		struct stat buf;
-		if ( ( lstat( tempbuf, &buf ) == 0 ) && S_ISDIR( buf.st_mode ) )
+		if ( ( lstat( temp.c_str(), &buf ) == 0 ) && S_ISDIR( buf.st_mode ) )
 #else
 		if (de->d_type == DT_DIR)
 #endif
