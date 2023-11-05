2a00ebd0a3c599c705dd19bc5564e8fbc2d869d2
TheBrokenRail <connor24nolan@live.com>
2023-11-05 16:11:09 -0500

    Fix Immersive Mode On Older Android

--- platforms/sdl/android/app/src/main/java/io/github/reminecraftpe/MainActivity.java
+++ platforms/sdl/android/app/src/main/java/io/github/reminecraftpe/MainActivity.java
@@ -2,14 +2,13 @@ package io.github.reminecraftpe;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 import android.view.WindowManager;
 
 import org.libsdl.app.LimitedSDLActivity;
 
 public class MainActivity extends LimitedSDLActivity {
+    private final ImmersiveModeStrategy immersiveModeStrategy = ImmersiveModeStrategy.create(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,33 +17,14 @@ public class MainActivity extends LimitedSDLActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         }
+        immersiveModeStrategy.onCreate();
     }
 
     @Override
-    @SuppressWarnings("deprecation")
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
 
         // Fullscreen
-        if (hasFocus) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                getWindow().setDecorFitsSystemWindows(false);
-                WindowInsetsController controller = getWindow().getInsetsController();
-                if (controller != null) {
-                    controller.hide(WindowInsets.Type.systemBars());
-                    controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-                }
-            } else {
-                getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                    View.SYSTEM_UI_FLAG_FULLSCREEN |
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                    View.INVISIBLE
-                );
-            }
-        }
+        immersiveModeStrategy.onWindowFocusChanged(hasFocus);
     }
 }
