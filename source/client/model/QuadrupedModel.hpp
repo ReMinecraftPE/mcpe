166bcf708e53ffef78913f9fe0e0427dd6bf2271
iProgramInCpp <iprogramincpp@gmail.com>
2023-12-05 19:08:11 +0200

    * Add Base QuadrupedModel

--- source/client/model/QuadrupedModel.hpp
+++ source/client/model/QuadrupedModel.hpp
@@ -9,11 +9,24 @@
 
 #include "Model.hpp"
 
-/*
-
 class QuadrupedModel : public Model
 {
+public:
+	QuadrupedModel(int, float);
+	~QuadrupedModel();
 
+	void render(float, float, float, float, float, float) override;
+	void setupAnim(float, float, float, float, float, float) override;
+
+private:
+	bool m_bIsBaby;
+	ModelPart m_head;
+	ModelPart m_unknown;
+	ModelPart m_body;
+	ModelPart m_leg1;
+	ModelPart m_leg2;
+	ModelPart m_leg3;
+	ModelPart m_leg4;
+	float field_28C;
+	float field_290;
 };
-
-*/
