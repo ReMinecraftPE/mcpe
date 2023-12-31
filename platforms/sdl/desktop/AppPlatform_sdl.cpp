783e2b5750860a2ced7076f36cfeea66ef6792df
f <48766067+Stommm@users.noreply.github.com>
2023-12-31 18:02:52 +0100

    Use SDL read/write operations

--- platforms/sdl/desktop/AppPlatform_sdl.cpp
+++ platforms/sdl/desktop/AppPlatform_sdl.cpp
@@ -200,12 +200,26 @@ bool AppPlatform_sdl::hasFileSystemAccess()
 
 std::string AppPlatform_sdl::getPatchData()
 {
-	std::ifstream ifs("assets/patches/patch_data.txt");
-	if (!ifs.is_open())
+
+	// Get Full Path
+    std::string realPath = getAssetPath("patches/patch_data.txt");
+
+	// Read File
+	SDL_RWops *io = SDL_RWFromFile(realPath.c_str(), "rb");
+	if (!io)
+	{
+		LOG_I("Couldn't open assets/patches/patch_data.txt");
 		return "";
+	}
 
-	std::stringstream ss;
-	ss << ifs.rdbuf();
+	Sint64 size = SDL_RWsize(io);
+	char *buff = static_cast<char*>(calloc(1,size+1));
+	SDL_RWread(io, buff, size, 1);
+	SDL_RWclose(io);
 
-	return ss.str();
+	std::string result(buff);
+
+	free(buff);
+
+	return result;
 }
