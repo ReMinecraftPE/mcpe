7775a0f0dd363f66124e9f6c459acbf1acada9d6
iProgramInCpp <iprogramincpp@gmail.com>
2024-04-29 21:21:06 +0300

    * Allow directly specifying use of Win32 threads

--- source/common/CThread.hpp
+++ source/common/CThread.hpp
@@ -12,12 +12,14 @@
 
 #if defined(_WIN32)
 
+#ifndef USE_WIN32_THREADS
 #if defined(_XBOX) || defined(USE_OLD_CPP)
 // USE_WIN32_THREADS - Use a Win32 implementation of threads instead of using pthread
 #define USE_WIN32_THREADS
 #else
 // USE_CPP11_THREADS - Use a C++11 implementation of threads instead of using pthread
 #define USE_CPP11_THREADS
+#endif
 #endif
 
 #else
