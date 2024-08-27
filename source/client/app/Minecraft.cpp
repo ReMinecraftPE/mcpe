876abab9937e24ea7a3baa427ed00f19159ebe2a
KoopaOnGit <tboi86025@gmail.com>
2024-08-27 15:08:28 +0200

    Update Minecraft.cpp

--- source/client/app/Minecraft.cpp
+++ source/client/app/Minecraft.cpp
@@ -1163,6 +1163,8 @@ int Minecraft::getFpsIntlCounter()
 
 void Minecraft::leaveGame(bool bCopyMap)
 {
+	Keyboard::setBlocked(true);
+
 	m_bPreparingLevel = false;
 	m_pRakNetInstance->disconnect();
 	m_pMobPersp = nullptr;
