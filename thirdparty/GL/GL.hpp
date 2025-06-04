3f586e878256311a5abf590966f9928cdaf6ab14
Brent <43089001+BrentDaMage@users.noreply.github.com>
2025-06-04 01:19:53 -0500

    Update GL.hpp

--- thirdparty/GL/GL.hpp
+++ thirdparty/GL/GL.hpp
@@ -38,7 +38,9 @@
 
 	// https://discourse.libsdl.org/t/opengl-es2-support-on-windows/20177/10
 	// float on GLES for performance reasons (mobile hardware) rather than double precision on GL
-	#define glClearColor glClearColorf
+	#if GL_ES_VERSION_2_0
+		#define glClearColor glClearColorf
+	#endif
 	#define glClearDepth glClearDepthf
 	#define glDepthRange glDepthRangef
 #else
