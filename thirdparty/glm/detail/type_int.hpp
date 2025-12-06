a6bfa3cd61725683fed8d0254a950d3dc9903305
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2025-12-05 20:36:52 -0600

    Update GLM type_int

--- thirdparty/glm/detail/type_int.hpp
+++ thirdparty/glm/detail/type_int.hpp
@@ -39,15 +39,15 @@ namespace glm{
 namespace detail
 {
 #	if GLM_HAS_EXTENDED_INTEGER_TYPE
-		typedef std::int8_t					int8;
-		typedef std::int16_t				int16;
-		typedef std::int32_t				int32;
-		typedef std::int64_t				int64;
+		typedef int8_t					    int8;
+		typedef int16_t				        int16;
+		typedef int32_t				        int32;
+		typedef int64_t				        int64;
 	
-		typedef std::uint8_t				uint8;
-		typedef std::uint16_t				uint16;
-		typedef std::uint32_t				uint32;
-		typedef std::uint64_t				uint64;
+		typedef uint8_t				        uint8;
+		typedef uint16_t				    uint16;
+		typedef uint32_t				    uint32;
+		typedef uint64_t				    uint64;
 #	else
 #		if(defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)) // C99 detected, 64 bit types available
 			typedef int64_t					sint64;
