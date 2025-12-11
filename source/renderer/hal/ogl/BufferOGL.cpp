a8125faf9853a97dac5604398612a04643c21f49
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2025-12-10 18:29:16 -0600

    Modify GLES 1 buffer workaround check

--- source/renderer/hal/ogl/BufferOGL.cpp
+++ source/renderer/hal/ogl/BufferOGL.cpp
@@ -127,14 +127,15 @@ void BufferOGL::updateBuffer(RenderContext& context, unsigned int stride, void*&
     // to be rendered, so perhaps some day...
     // Additionally, we could try holding the vertex buffer data in memory and pass
     // it in the draw call, as supposedly not even using buffers is faster.
-    bool useGLES1Workaround = false;
-#if MC_PLATFORM_IOS || MC_PLATFORM_ANDROID
-    useGLES1Workaround = true;
+#ifdef GL_VERSION_ES_CM_1_0
+#define GLES1_WORKAROUND true
+#else
+#define GLES1_WORKAROUND false
 #endif
     
     const unsigned int size = count * stride;
 
-    if (!useGLES1Workaround && size <= m_internalSize)
+    if (!GLES1_WORKAROUND && size <= m_internalSize)
         xglBufferSubData(m_target, m_bufferOffset, size, data);
     else
         resizeBuffer(context, data, size);
