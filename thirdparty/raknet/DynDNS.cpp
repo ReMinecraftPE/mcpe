a13a1cbdcd9b194c786f0b501808388323020bf4
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-22 20:28:03 -0800

    always true warn

--- thirdparty/raknet/DynDNS.cpp
+++ thirdparty/raknet/DynDNS.cpp
@@ -219,7 +219,7 @@ void DynDNS::Update(void)
 				existingHost[0]=0;
 				// Resolve DNS we are setting. If equal to current then abort
 				RakNetSocket2::DomainNameToIP(host.C_String(), existingHost);
-				if (existingHost && strcmp(existingHost, myIPStr)==0)
+				if (strcmp(existingHost, myIPStr)==0)
 				{
 					// DynDNS considers setting the IP to what it is already set abuse
 					tcp->DeallocatePacket(packet);
