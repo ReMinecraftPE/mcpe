a7f37c62c4d536856af10398eb2f8d8289bd3654
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-04 17:50:39 -0500

    fix

--- platforms/sound/directsound/CustomSoundSystem.hpp
+++ platforms/sound/directsound/CustomSoundSystem.hpp
@@ -14,9 +14,11 @@
 #include <stdexcept>
 #include <vector>
 
+#ifdef _MSVC_VER
 #pragma comment(lib, "dsound.lib")
 #pragma comment(lib, "dxguid.lib")
 #pragma comment(lib, "winmm.lib")
+#endif
 
 #include <mmsystem.h>
 #include <dsound.h>
