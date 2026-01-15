6fb1b258bca2de5196fc6f316b1de78ad3f056fa
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-15 12:49:46 -0500

    vsnprintf workaround testing

--- compat/vsnprintf.cpp
+++ compat/vsnprintf.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-extern "C" {
-	int remcpe_vsnprintf(char* buf, size_t size, const char* format, va_list ap)
-	{
-		return vsnprintf(buf, size, format, ap);
-	}
+int remcpe_vsnprintf(char* buf, size_t size, const char* format, va_list ap)
+{
+	return vsnprintf(buf, size, format, ap);
 }
