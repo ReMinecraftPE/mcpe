57121be45ab64fd4b115148ea4b5787c7e2e346e
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-15 13:56:08 -0500

    remove vsnprintf workaround

--- compat/vsnprintf.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int remcpe_vsnprintf(char* buf, size_t size, const char* format, va_list ap)
-{
-	return vsnprintf(buf, size, format, ap);
-}
--- compat/vsnprintf.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#ifndef vsnprintf
-#include <stdarg.h>
-extern int remcpe_vsnprintf(char *, size_t, const char *, va_list ap);
-#define vsnprintf remcpe_vsnprintf
-#endif
