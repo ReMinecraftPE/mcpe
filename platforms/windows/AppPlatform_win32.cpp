156140e08c25a3e079da0045e5fde67c8c3e6097
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-19 16:57:16 -0500

    fix

--- platforms/windows/AppPlatform_win32.cpp
+++ platforms/windows/AppPlatform_win32.cpp
@@ -201,9 +201,9 @@ bool AppPlatform_win32::hasFileSystemAccess()
 	return true;
 }
 
-AssetFile AppPlatform_win32::readAssetFile(const std::string& str, bool quiet, const std::vector<std::string>& resourcepacks) const
+AssetFile AppPlatform_win32::readAssetFile(const std::string& str, bool quiet) const
 {
-	std::string path = getAssetPath(str, resourcepacks);
+	std::string path = getAssetPath(str);
 	std::ifstream ifs(path, std::ios::binary | std::ios::ate);
 	if (!ifs.is_open())
 		return AssetFile();
