87ef213c41ef21839da181788fd1e594bab975cb
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2026-01-19 03:02:44 -0600

    Fixed a bug in BufferD3DX::updateBuffer functions

--- source/renderer/hal/d3d11/BufferD3D11.cpp
+++ source/renderer/hal/d3d11/BufferD3D11.cpp
@@ -172,6 +172,7 @@ void BufferD3D11::updateBuffer(RenderContext& context, unsigned int stride, void
     {
         d3dDeviceContext->Unmap(**m_buffer, 0);
         m_buffer.release();
+        // Mojang did "stride * count" for the stride argument here, probably a bug
         createDynamicBuffer(context, stride * count, data, count, m_bufferType);
     }
     else
