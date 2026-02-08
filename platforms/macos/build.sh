3e369c0b671ad6c8c91f5e929ad5035ba72205f4
Un1q32 <joey.t.reinhart@gmail.com>
2026-02-08 03:26:17 -0500

    fix

--- platforms/macos/build.sh
+++ platforms/macos/build.sh
@@ -95,6 +95,7 @@ printf '#!/bin/sh\n
         exec %sclang "$@"\n' "$ccache " > bin/remcpe-clang
 printf '#!/bin/sh\n
         exec %sclang++ "$@"\n' "$ccache " > bin/remcpe-clang++
+chmod +x remcpe-clang remcpe-clang++
 
 if [ -n "$outdated_toolchain" ]; then
     # this step is needed even on macOS since newer versions of Xcode will straight up not let you link for old macOS versions anymore
