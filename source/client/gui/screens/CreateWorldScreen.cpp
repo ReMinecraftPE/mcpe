a3e7ab0f8fc42a165b60a67c4699171a929cb17e
rep.stosx <95462744+rep-stosd@users.noreply.github.com>
2026-02-10 10:12:16 -0500

    Refactor CreateWorldScreen and add game type button

--- source/client/gui/screens/CreateWorldScreen.cpp
+++ source/client/gui/screens/CreateWorldScreen.cpp
@@ -11,15 +11,61 @@
 #include "ProgressScreen.hpp"
 #include "common/Util.hpp"
 
-static char g_CreateWorldFilterArray[] = { '/','\n','\r','\x09','\0','\xC','`','?','*','\\','<','>','|','"',':' };
-
 CreateWorldScreen::CreateWorldScreen() :
 	m_textName(this, 1, 0, 0, 0, 0, "", "Unnamed world"),
 	m_textSeed(this, 2, 0, 0, 0, 0, ""),
+	m_btnGameType(5, "Game Type"),
 	m_btnBack(3, "Cancel"),
 	m_btnCreate(4, "Create New World")
 {
 }
+
+#define CRAMPED() (100 + 32 + 58 > m_height)
+
+void CreateWorldScreen::init()
+{
+	m_textName.m_width  = m_textSeed.m_width  = 200;
+	m_textName.m_height = m_textSeed.m_height = 20;
+	m_textName.m_xPos   = m_textSeed.m_xPos   = (m_width - 200) / 2;
+	m_textName.m_yPos = 60;
+	m_textSeed.m_yPos = 100;
+
+	m_btnGameType.m_yPos  = 140;
+	m_btnCreate.m_yPos  = m_height - 56;
+	m_btnBack.m_yPos    = m_height - 30;
+	m_btnBack.m_width   = m_btnCreate.m_width  = m_btnGameType.m_width   = 200;
+	m_btnBack.m_height  = m_btnCreate.m_height = m_btnGameType.m_height  = 20;
+	
+	m_btnBack.m_xPos    = m_btnCreate.m_xPos   = m_btnGameType.m_xPos = m_width / 2 - 200 / 2;
+
+	m_textInputs.push_back(&m_textName);
+	m_textInputs.push_back(&m_textSeed);
+	m_buttons.push_back(&m_btnGameType);
+	m_buttons.push_back(&m_btnBack);
+	m_buttons.push_back(&m_btnCreate);
+	m_buttonTabList.push_back(&m_btnGameType);
+	m_buttonTabList.push_back(&m_btnBack);
+	m_buttonTabList.push_back(&m_btnCreate);
+	m_textName.init(m_pFont);
+	m_textSeed.init(m_pFont);
+
+	// 100 - yPos of textSeed
+	// 32  - offset + height of "Leave blank for random" text
+	// 58  - approximately the Y position of the create button
+	bool crampedMode = CRAMPED();
+	if (crampedMode)
+	{
+		// crush everything down as much as we can
+		m_textName.m_yPos = 40;
+		m_textSeed.m_yPos = 80;
+		m_btnCreate.m_yPos += 10;
+		m_btnBack.m_yPos += 5;
+	}
+
+	m_selectedGameType = 0;
+}
+
+static char g_CreateWorldFilterArray[] = { '/','\n','\r','\x09','\0','\xC','`','?','*','\\','<','>','|','"',':' };
 
 static std::string GetUniqueLevelName(LevelStorageSource* pSource, const std::string& in)
 {
@@ -48,14 +94,13 @@ static std::string GetUniqueLevelName(LevelStorageSource* pSource, const std::st
 	return out;
 }
 
-void CreateWorldScreen::_buttonClicked(Button* pButton)
+void CreateWorldScreen::buttonClicked(Button* pButton)
 {
-	if (pButton->getId() == m_btnBack.getId())
+	if (pButton->m_buttonId == m_btnBack.m_buttonId)
 	{
-		handleBackEvent(false);
+		m_pMinecraft->setScreen(new SelectWorldScreen);
 	}
-
-	if (pButton->getId() == m_btnCreate.getId())
+	else if (pButton->m_buttonId == m_btnCreate.m_buttonId)
 	{
 		std::string nameStr = m_textName.getText();
 		std::string seedStr = m_textSeed.getText();
@@ -63,7 +108,7 @@ void CreateWorldScreen::_buttonClicked(Button* pButton)
 		std::string levelNickname = Util::stringTrim(nameStr);
 		std::string levelUniqueName = levelNickname;
 
-		for (size_t i = 0; i < sizeof(g_CreateWorldFilterArray); i++)
+		for (int i = 0; i < sizeof(g_CreateWorldFilterArray); i++)
 		{
 			std::string str;
 			str.push_back(g_CreateWorldFilterArray[i]);
@@ -84,67 +129,29 @@ void CreateWorldScreen::_buttonClicked(Button* pButton)
 				seed = Util::hashCode(seedThing);
 		}
 
-		LevelSettings levelSettings(seed);
+		LevelSettings levelSettings(seed, (GameType)m_selectedGameType);
 		m_pMinecraft->selectLevel(levelUniqueName, levelNickname, levelSettings);
 	}
-}
-
-#define CRAMPED() (100 + 32 + 58 > m_height)
-
-void CreateWorldScreen::init()
-{
-	m_textName.m_width  = m_textSeed.m_width  = 200;
-	m_textName.m_height = m_textSeed.m_height = 20;
-	m_textName.m_xPos   = m_textSeed.m_xPos   = (m_width - 200) / 2;
-	m_textName.m_yPos = 60;
-	m_textSeed.m_yPos = 100;
-
-	m_btnCreate.m_yPos  = m_height - 56;
-	m_btnBack.m_yPos    = m_height - 30;
-	m_btnBack.m_width   = m_btnCreate.m_width  = 200;
-	m_btnBack.m_height  = m_btnCreate.m_height = 20;
-	
-	m_btnBack.m_xPos   = m_width / 2 - 200 / 2;
-	m_btnCreate.m_xPos = m_width / 2 - 200 / 2;
-
-	_addElement(m_textName);
-	_addElement(m_textSeed);
-	_addElement(m_btnCreate);
-	_addElement(m_btnBack);
-	m_textName.init(m_pFont);
-	m_textSeed.init(m_pFont);
-
-	// 100 - yPos of textSeed
-	// 32  - offset + height of "Leave blank for random" text
-	// 58  - approximately the Y position of the create button
-	bool crampedMode = CRAMPED();
-	if (crampedMode)
+	else if (pButton->m_buttonId == m_btnGameType.m_buttonId)
 	{
-		// crush everything down as much as we can
-		m_textName.m_yPos = 40;
-		m_textSeed.m_yPos = 80;
-		m_btnCreate.m_yPos += 10;
-		m_btnBack.m_yPos += 5;
+		m_selectedGameType = (m_selectedGameType + 1) % (GAME_TYPES_MAX + 1);
 	}
+
 }
 
-void CreateWorldScreen::render(float f)
+void CreateWorldScreen::render(int mouseX, int mouseY, float f)
 {
+	const GameType gameType = (GameType)m_selectedGameType;
+	const std::string gameTypeStr = GameTypeConv::GameTypeDescriptionToNonLocString(gameType);
+	m_btnGameType.m_text = GameTypeConv::GameTypeToNonLocString(gameType);
+
 	renderBackground();
-	Screen::render(f);
+	Screen::render(mouseX, mouseY, f);
 
 	drawCenteredString(*m_pFont, "Create New World", m_width / 2, CRAMPED() ? 10 : 30, 0xFFFFFF);
 	drawString(*m_pFont, "World name",                    m_textName.m_xPos, m_textName.m_yPos - 10, 0xDDDDDD);
 	drawString(*m_pFont, "Seed for the World Generator",  m_textSeed.m_xPos, m_textSeed.m_yPos - 10, 0xDDDDDD);
 	drawString(*m_pFont, "Leave blank for a random seed", m_textSeed.m_xPos, m_textSeed.m_yPos + 22, 0x999999);
-}
 
-bool CreateWorldScreen::handleBackEvent(bool b)
-{
-	if (!b)
-	{
-		m_pMinecraft->setScreen(new SelectWorldScreen);
-	}
-
-	return true;
+	drawString(*m_pFont, gameTypeStr.c_str(), m_btnGameType.m_xPos, m_btnGameType.m_yPos + 22, 0x999999);
 }
