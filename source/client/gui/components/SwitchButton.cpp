6a7ec814ddad6eeb039071e8c0b87d3681faf411
Vimd0 <minecraftjb22@gmail.com>
2026-02-11 22:07:50 -0500

    more

--- source/client/gui/components/SwitchButton.cpp
+++ source/client/gui/components/SwitchButton.cpp
@@ -2,8 +2,8 @@
 #include "client/locale/Language.hpp"
 #include "renderer/ShaderConstants.hpp"
 
-#define C_ON_OFF_SWITCH_WIDTH (32)
-#define C_ON_OFF_SWITCH_HEIGHT (18)
+#define C_ON_OFF_SWITCH_WIDTH (38)
+#define C_ON_OFF_SWITCH_HEIGHT (19)
 
 SwitchButton::SwitchButton(int id, int x, int y, BoolOption* option, const std::string& message) : Button(id, x, y, 150, 20, message),
 	m_pOption(option)
@@ -31,9 +31,19 @@ void SwitchButton::render(Minecraft* mc, const MenuPointer& pointer)
 
 	currentShaderColor = m_color;
 
-	// Obs: The color setting that affected disabled items' texts will also affect this.
+	mc->m_pTextures->loadAndBindTexture("gui/touchgui.png");
 
-	mc->m_pTextures->loadAndBindTexture("gui/gui_custom.png");
+	int uOffset = getOption().get() ? 160 + C_ON_OFF_SWITCH_WIDTH : 160;
+	int vOffset = 206;
 
-	blit(m_xPos + m_width - C_ON_OFF_SWITCH_WIDTH - 6, m_yPos + (m_height - C_ON_OFF_SWITCH_HEIGHT) / 2, 0, getOption().get() ? 0 : 18, C_ON_OFF_SWITCH_WIDTH, C_ON_OFF_SWITCH_HEIGHT, C_ON_OFF_SWITCH_WIDTH, C_ON_OFF_SWITCH_HEIGHT);
+	blit(
+		m_xPos + m_width - C_ON_OFF_SWITCH_WIDTH - 6, 
+		m_yPos + (m_height - C_ON_OFF_SWITCH_HEIGHT) / 2, 
+		uOffset, 
+		vOffset, 
+		C_ON_OFF_SWITCH_WIDTH, 
+		C_ON_OFF_SWITCH_HEIGHT, 
+		C_ON_OFF_SWITCH_WIDTH, 
+		C_ON_OFF_SWITCH_HEIGHT
+	);
 }
