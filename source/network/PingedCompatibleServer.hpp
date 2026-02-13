2957fd44112eb7901c7d254e383919ceab851186
Brent <43089001+BrentDaMage@users.noreply.github.com>
2026-02-12 23:01:30 -0800

    Fixed Xbox 360 Networking (#459)

--- source/network/PingedCompatibleServer.hpp
+++ source/network/PingedCompatibleServer.hpp
@@ -11,11 +11,26 @@
 #include <string>
 #include "RakNetTypes.h"
 #include "RakString.h"
-
+#include "compat/PlatformDefinitions.h"
+#include "common/Util.hpp"
 
 struct PingedCompatibleServer
 {
 	RakNet::RakString m_name;
 	RakNet::SystemAddress m_address;
 	int m_lastPinged;
+
+	std::string getAddressStr() const
+	{
+#if MC_PLATFORM_XBOX
+		return Util::format("%d.%d.%d.%d|%hu", m_address.address.addr4.sin_addr.S_un.S_un_b.s_b1
+		                                     , m_address.address.addr4.sin_addr.S_un.S_un_b.s_b2
+		                                     , m_address.address.addr4.sin_addr.S_un.S_un_b.s_b3
+		                                     , m_address.address.addr4.sin_addr.S_un.S_un_b.s_b4
+			                                 , m_address.GetPort()
+		);
+#else
+		return std::string(m_address.ToString());
+#endif
+	}
 };
